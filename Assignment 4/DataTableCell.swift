76800b9a43a909ab582d697c69379f9e5837d987
BDOTS1806 <39936675+Bdots1806@users.noreply.github.com>
2022-11-10 18:57:18 -0500

    Ios

---
+++ Assignment 4/DataTableCell.swift
@@ -0,0 +1,16 @@
+//
+//  DataTableCell.swift
+//  Assignment 4
+//
+//  Created by user214269 on 11/10/22.
+//
+
+import UIKit
+
+class DataTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
